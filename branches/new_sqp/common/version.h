225a066e9f87083f8c3a4b551ddc59f5cf829434
russellrice <russellrice@b0bcef46-2027-0410-bd56-ab726e9a1d04>
2008-08-22 13:10:51 +0000

    Create a branch for the new server query protocol implementation
    git-svn-id: http://odamex.net/svn/root@1108 b0bcef46-2027-0410-bd56-ab726e9a1d04

---
+++ branches/new_sqp/common/version.h
@@ -0,0 +1,56 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 2006-2008 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	Source versioning
+//
+//-----------------------------------------------------------------------------
+
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#if _MSC_VER == 1200
+// MSVC6, disable broken warnings about truncated stl lines
+#pragma warning(disable:4786)
+#endif
+
+// Lots of different representations for the version number
+#define CONFIGVERSIONSTR "40"
+#define GAMEVER (0*256+41)
+
+#define DOTVERSIONSTR "0.4.1"
+
+#define SERVERMAJ (gameversion / 256)
+#define SERVERMIN ((gameversion % 256) / 10)
+#define SERVERREL ((gameversion % 256) % 10)
+#define CLIENTMAJ (GAMEVER / 256)
+#define CLIENTMIN ((GAMEVER % 256) / 10)
+#define CLIENTREL ((GAMEVER % 256) % 10)
+
+// denis - per-file svn version stamps
+class file_version
+{
+public:
+	file_version(const char *uid, const char *id, const char *p, int l, const char *t, const char *d);
+};
+
+#define VERSION_CONTROL(uid, id) static file_version file_version_unique_##uid(#uid, id, __FILE__, __LINE__, __TIME__, __DATE__);
+
+#endif //__VERSION_H__
+
+
