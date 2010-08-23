JohnnyonFlame/odamex

commit 09466d72e8720ce1d07ff743c395e502b43c6583

Manual door activated lighting now works, fixes bug 206, based on prboom+.  Thanks Ladna!
git-svn-id: http://odamex.net/svn/root@1750 b0bcef46-2027-0410-bd56-ab726e9a1d04

trunk/common/p_doors.cpp
@@ -31,6 +31,8 @@
 #include "dstrings.h"
 #include "c_console.h"
 
+#include "p_spec.h"
+
 IMPLEMENT_SERIAL (DDoor, DMovingCeiling)
 
 DDoor::DDoor ()
@@ -114,9 +116,18 @@ void DDoor::RunThink ()
 		}
 		break;
 		
-	case -1:
+    case -1:
 		// DOWN
-		res = MoveCeiling (m_Speed, m_Sector->floorheight, false, m_Direction);
+        res = MoveCeiling (m_Speed, m_Sector->floorheight, false, m_Direction);
+        if (m_LightTag)
+        {
+            EV_LightTurnOnPartway(m_Line->id,
+                FixedDiv(
+                    m_Sector->ceilingheight - m_Sector->floorheight,
+                    m_TopHeight - m_Sector->floorheight
+                )
+            );
+        }
 		if (res == pastdest)
 		{
 			//S_StopSound (m_Sector->soundorg);
@@ -136,6 +147,10 @@ void DDoor::RunThink ()
 			default:
 				break;
 			}
+            if (m_LightTag)
+            {
+                EV_LightTurnOnPartway(m_Line->id, 0);
+            }
 		}
 		else if (res == crushed)
 		{
@@ -156,6 +171,15 @@ void DDoor::RunThink ()
 		// UP
 		res = MoveCeiling (m_Speed, m_TopHeight, false, m_Direction);
 		
+        if (m_LightTag)
+        {
+            EV_LightTurnOnPartway(m_Line->id,
+                FixedDiv(
+                    m_Sector->ceilingheight - m_Sector->floorheight,
+                    m_TopHeight - m_Sector->floorheight
+                )
+            );
+        }
 		if (res == pastdest)
 		{
 			//S_StopSound (m_Sector->soundorg);
@@ -175,6 +199,10 @@ void DDoor::RunThink ()
 			default:
 				break;
 			}
+            if (m_LightTag)
+            {
+                EV_LightTurnOnPartway(m_Line->id, FRACUNIT);
+            }
 		}
 		break;
 	}
@@ -212,12 +240,14 @@ DDoor::DDoor (sector_t *sector)
 //		and made them more general to support the new specials.
 
 // [RH] SpawnDoor: Helper function for EV_DoDoor
-DDoor::DDoor (sector_t *sec, EVlDoor type, fixed_t speed, int delay)
+DDoor::DDoor (sector_t *sec, line_t *ln, EVlDoor type, fixed_t speed, int delay)
 	: DMovingCeiling (sec)
 {
 	m_Type = type;
 	m_TopWait = delay;
 	m_Speed = speed;
+    m_Line = ln;
+    m_LightTag = m_Line->id;
 
 	switch (type)
 	{
@@ -253,6 +283,7 @@ BOOL EV_DoDoor (DDoor::EVlDoor type, line_t *line, AActor *thing,
 	BOOL		rtn = false;
 	int 		secnum;
 	sector_t*	sec;
+    DDoor *door;
 
 	if (lock && thing && !P_CheckKeys (thing->player, lock, tag))
 		return false;
@@ -276,7 +307,9 @@ BOOL EV_DoDoor (DDoor::EVlDoor type, line_t *line, AActor *thing,
 		// if door already has a thinker, use it
 		if (sec->ceilingdata && sec->ceilingdata->IsKindOf (RUNTIME_CLASS(DDoor)))
 		{
-			DDoor *door = static_cast<DDoor *>(sec->ceilingdata);	
+			door = static_cast<DDoor *>(sec->ceilingdata);	
+            door->m_LightTag = line->id;
+            door->m_Line = line;
 			
 			// ONLY FOR "RAISE" DOORS, NOT "OPEN"s
 			if (door->m_Type == DDoor::doorRaise && type == DDoor::doorRaise)
@@ -324,8 +357,14 @@ BOOL EV_DoDoor (DDoor::EVlDoor type, line_t *line, AActor *thing,
 				return true;
 			}
 		}
-		if (new DDoor (sec, type, speed, delay))
+        else
+        {
+            door = new DDoor(sec, line, type, speed, delay);
+        }
+		if (door)
+        {
 			rtn = true;
+        }
 	}
 	else
 	{	// [RH] Remote door
@@ -338,7 +377,7 @@ BOOL EV_DoDoor (DDoor::EVlDoor type, line_t *line, AActor *thing,
 			if (sec->ceilingdata)
 				continue;
 
-			if (new DDoor (sec, type, speed, delay))
+			if (new DDoor (sec, line, type, speed, delay))
 				rtn = true;
 		}
 				

trunk/common/p_lights.cpp
@@ -405,6 +405,61 @@ void EV_LightTurnOn (int tag, int bright)
 	}
 }
 
+/* killough 10/98:
+ *
+ * EV_LightTurnOnPartway()
+ *
+ * Turn sectors tagged to line lights on to specified or max neighbor level
+ *
+ * Passed the activating line, and a light level fraction between 0 and 1.
+ * Sets the light to min on 0, max on 1, and interpolates in-between.
+ * Used for doors with gradual lighting effects.
+ *
+ * Returns true
+ */
+// int EV_LightTurnOnPartway(line_t *line, fixed_t level)
+int EV_LightTurnOnPartway(int tag, int level)
+{
+    int i;
+
+    if (level < 0)          // clip at extremes
+    {
+        level = 0;
+    }
+    if (level > FRACUNIT)
+    {
+        level = FRACUNIT;
+    }
+
+    // search all sectors for ones with same tag as activating line
+    // for (i = -1; (i = P_FindSectorFromLineTag(line,i)) >= 0;)
+    for (i = -1; (i = P_FindSectorFromTag(tag, i)) >= 0;)
+    {
+        sector_t *temp, *sector = sectors+i;
+        int j;
+        int bright = 0;
+        int min = sector->lightlevel;
+        for (j = 0; j < sector->linecount; j++)
+        {
+            if ((temp = getNextSector(sector->lines[j], sector)))
+            {
+                if (temp->lightlevel > bright)
+                {
+                    bright = temp->lightlevel;
+                }
+                if (temp->lightlevel < min)
+                {
+                    min = temp->lightlevel;
+                }
+            }
+        }
+        // Set level in-between extremes
+        sector->lightlevel =
+            (level * bright + (FRACUNIT-level) * min) >> FRACBITS;
+    }
+    return 1;
+}
+
 
 //
 // [RH] New function to adjust tagged sectors' light levels

trunk/common/p_spec.h
@@ -379,6 +379,7 @@ void	EV_StartLightStrobing (int tag, int utics, int ltics);
 void	EV_TurnTagLightsOff (int tag);
 void	EV_LightTurnOn (int tag, int bright);
 void	EV_LightChange (int tag, int value);
+int     EV_LightTurnOnPartway(int tag, int level);
 
 void	P_SpawnGlowingLight (sector_t *sector);
 
@@ -552,7 +553,8 @@ public:
 	};
 
 	DDoor (sector_t *sector);
-	DDoor (sector_t *sec, EVlDoor type, fixed_t speed, int delay);
+	// DDoor (sector_t *sec, EVlDoor type, fixed_t speed, int delay);
+    DDoor (sector_t *sec, line_t *ln, EVlDoor type, fixed_t speed, int delay);
 
 	void RunThink ();
 protected:
@@ -568,6 +570,10 @@ protected:
 	// (keep in case a door going down is reset)
 	// when it reaches 0, start going down
 	int 		m_TopCountdown;
+
+    line_t      *m_Line;
+
+    int         m_LightTag;
 
 	void DoorSound (bool raise) const;
 

commit 39a599bde5ea50d824bd5b6388706aa87ce9014c

Don't mess around with NULL pointers. Fixes crashing demos introduced in r1750.
git-svn-id: http://odamex.net/svn/root@1773 b0bcef46-2027-0410-bd56-ab726e9a1d04

trunk/common/p_doors.cpp
@@ -247,7 +247,9 @@ DDoor::DDoor (sector_t *sec, line_t *ln, EVlDoor type, fixed_t speed, int delay)
 	m_TopWait = delay;
 	m_Speed = speed;
     m_Line = ln;
-    m_LightTag = m_Line->id;
+
+	if(m_Line->id)
+		m_LightTag = m_Line->id;
 
 	switch (type)
 	{